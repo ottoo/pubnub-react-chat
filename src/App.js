8c0084a013101d8ddbe89f7b21ae078071cb8feb
Otto Kivikärki <otto.kivikarki@vincit.fi>
2019-08-05 17:24:39 +0300

    Improve App component

--- src/App.js
+++ src/App.js
@@ -3,35 +3,46 @@ import { ChatContext } from "./ChatContext";
 
 import "./App.css";
 
-function App() {
-  const { messages, sendMessage } = React.useContext(ChatContext);
+const MessageList = ({ messages }) => (
+  <ul className="messagelist">
+    {messages.map((m, i) => (
+      <li key={i}>
+        <div>{m.timestamp}</div>
+        <div>{m.publisher}</div>
+        <div>{m.text}</div>
+      </li>
+    ))}
+  </ul>
+);
+
+const ChatInputForm = ({ sendMessage }) => {
   const [inputValue, setInputValue] = React.useState("");
 
   return (
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        sendMessage({ text: inputValue });
+        setInputValue("");
+      }}
+    >
+      <input
+        placeholder="Send a message"
+        onChange={e => setInputValue(e.target.value)}
+        value={inputValue}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+function App() {
+  const { messages, sendMessage } = React.useContext(ChatContext);
+
+  return (
     <div className="App">
-      <ul className="messagelist">
-        {messages.map((m, i) => (
-          <li key={i}>
-            <div>{m.timestamp}</div>
-            <div>{m.publisher}</div>
-            <div>{m.text}</div>
-          </li>
-        ))}
-      </ul>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          sendMessage({ text: inputValue });
-          setInputValue("");
-        }}
-      >
-        <input
-          placeholder="Send a message"
-          onChange={e => setInputValue(e.target.value)}
-          value={inputValue}
-        />
-        <button type="submit">Send</button>
-      </form>
+      <MessageList messages={messages} />
+      <ChatInputForm sendMessage={sendMessage} />
     </div>
   );
 }
